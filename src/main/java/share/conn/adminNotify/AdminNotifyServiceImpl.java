7d120129ae29b2ae955a2cdb30fa94f0e1ef326d
Sinbatd <53296433+Sinbatd@users.noreply.github.com>
2019-07-26 18:13:06 +0900

    미완성
    ㅇㅇ

--- src/main/java/share/conn/adminNotify/AdminNotifyServiceImpl.java
+++ src/main/java/share/conn/adminNotify/AdminNotifyServiceImpl.java
@@ -1,5 +1,25 @@
 package share.conn.adminNotify;
 
-public class AdminNotifyServiceImpl {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+ @Service("adminNotifyService")
+public class AdminNotifyServiceImpl implements AdminNotifyService {
+	 
+	 
+	 
+ 
+	
+	@Resource(name = "adminNotifyDAO")
+	private AdminNotifyDAO adminNotifyDAO;
 
 }
+ 
+ 
