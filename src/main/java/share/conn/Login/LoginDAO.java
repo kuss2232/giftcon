2eec74e3e7db954b1ca7c858e380894325fd6a28
ezentube2 <>
2019-07-30 16:52:24 +0900

    07/30 login수정

--- src/main/java/share/conn/Login/LoginDAO.java
+++ src/main/java/share/conn/Login/LoginDAO.java
@@ -14,4 +14,21 @@ public class LoginDAO extends AbstractDAO{
 	public Map<String, Object> selectId(Map<String,Object> map)throws Exception{
 		return (Map<String, Object>)selectOne("memberInfo.selectMember", map);
 	}
+	
+	//아이디 찾기
+	@SuppressWarnings("unchecked")
+	public Map<String, Object> findId(Map<String, Object> map) throws Exception{
+		return (Map<String, Object>)selectOne("memberInfo.findId", map);
+	}
+	
+	//비밀번호 찾기
+	@SuppressWarnings("unchecked")
+	public Map<String, Object> findPwd(Map<String, Object> map)throws Exception{
+		return (Map<String, Object>)selectOne("memberInfo.findPwd", map);
+	}
+	
+	public void chgPw(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		update("memberInfo.chgPw", map);
+	}
 }
--- src/main/java/share/conn/Login/LoginService.java
+++ src/main/java/share/conn/Login/LoginService.java
@@ -5,4 +5,12 @@ import java.util.Map;
 public interface LoginService {
 
 	Map<String, Object> loginGo(Map<String, Object> map) throws Exception;
+
+	Map<String, Object> findId(Map<String, Object> map) throws Exception;
+
+	Map<String, Object> findPwd(Map<String, Object> map) throws Exception;
+
+	void chgPw(Map<String, Object> map) throws Exception;
+	
+	
 }
