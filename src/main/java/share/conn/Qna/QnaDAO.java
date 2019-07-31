29168c70d6fdd3e67152e63380de94f191f4e453
plsrmb <plsrmb@naver.com>
2019-07-31 17:11:45 +0900

    qna 임시저장

--- src/main/java/share/conn/Qna/QnaDAO.java
+++ src/main/java/share/conn/Qna/QnaDAO.java
@@ -1,5 +1,29 @@
 package share.conn.Qna;
 
-public class QnaDAO {
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.stereotype.Repository;
+
+import share.conn.giftcon.AbstractDAO;
+
+
+@Repository("qnaDAO")
+public class QnaDAO extends AbstractDAO{
+	
+	//QNA 등록하기
+	public void insertQna(Map<String, Object> map) throws Exception {	
+		insert("memberInfo.insertQna", map);
+	}
+	//QNA 목록 보기
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> qnaList(Map<String, Object> map) throws Exception {
+		return (List<Map<String, Object>>) selectList("memberInfo.findQnaList", map); 
+	}
+	
+	// QNA 상세보기
+	@SuppressWarnings("unchecked")
+	public Map<String, Object> qnaDetail(Map<String, Object> map) throws Exception {
+		return (Map<String, Object>) selectOne("memberInfo.qnaDetail", map);
+	}
 }
--- src/main/java/share/conn/Qna/QnaServiceImpl.java
+++ src/main/java/share/conn/Qna/QnaServiceImpl.java
@@ -1,5 +1,34 @@
 package share.conn.Qna;
 
-public class QnaServiceImpl {
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+
+@Service("qnaService")
+public class QnaServiceImpl implements QnaService{
+	
+	@Resource(name = "qnaDAO")
+	private QnaDAO qnaDAO;
+
+	//QNA 작성
+	@Override
+	public void insertQna(Map<String, Object> map) throws Exception {
+		qnaDAO.insertQna(map);
+	}
+	//QNA 목록
+	@Override
+	public List<Map<String, Object>> qnaList(Map<String, Object> map) throws Exception {
+		return qnaDAO.qnaList(map);
+	}
+	//QNA 상세보기
+	@Override
+	public Map<String, Object> qnaDetail(Map<String, Object> map) throws Exception {
+		Map<String, Object> qnaDetail = qnaDAO.qnaDetail(map);
+		return qnaDetail;
+	}
 
 }
