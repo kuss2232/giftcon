fcde1e88a0b6c8ead44a3bd3557aa11398185dde
kuss2232 <kuss2232@naver.com>
2019-08-01 18:03:46 +0900

    order

--- src/main/java/share/conn/adminOrder/AdminOrderService.java
+++ src/main/java/share/conn/adminOrder/AdminOrderService.java
@@ -1,5 +1,19 @@
 package share.conn.adminOrder;
 
-public interface AdminOrderService {
+import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+public interface AdminOrderService {
+	public List<Map<String,Object>> allOrderList() throws Exception;
+	
+	//전체 주문 수 보기
+	public int allOrderNumList() throws Exception;
+	
+	//주문 삭제
+	public void orderDelete(Map<String, Object> map) throws Exception;
+	
+	//관리자가 사용자 ID로 검색
+	public List<Map<String,Object>> adminOrderSearchID(Map<String,Object> map) throws Exception;
 }
