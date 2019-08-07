fd3e7871a3b80de95d4e44b3a3f671b4d8aac090
ezentube2 <>
2019-08-07 22:00:56 +0900

    10ho

--- src/main/java/share/conn/adminReview/AdminReviewDAO.java
+++ src/main/java/share/conn/adminReview/AdminReviewDAO.java
@@ -1,5 +1,38 @@
 package share.conn.adminReview;
 
-public class AdminReviewDAO {
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import share.conn.giftcon.AbstractDAO;
+
+@Repository("adminReviewDAO")
+public class AdminReviewDAO extends AbstractDAO{
+
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> reviewList(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return selectList("adminReview.selectReviewList", map);
+	}
+
+	public void reviewDelete(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		update("adminReview.deleteReview", map);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Map<String, Object> reviewCount(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return (Map<String, Object>)selectOne("adminReview.reviewCount", map);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> searchReviewList(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		System.out.println("들어오는지 확인222:::::"+map.get("SearchKeyword"));
+		System.out.println("서치넘 확인222:::::::::::"+ map.get("searchNum"));
+		return selectList("adminReview.selectSearchList", map);
+	}
 
 }
