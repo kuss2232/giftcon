739abb7bc6e504c16842f3c83135d35aaa40f17f
plsrmb <53296514+plsrmb@users.noreply.github.com>
2019-09-18 16:15:45 +0900

    Update MainController.java

--- src/main/java/share/conn/Main/MainController.java
+++ src/main/java/share/conn/Main/MainController.java
@@ -30,12 +30,18 @@ public class MainController {
 	@Resource(name="cartService")
 	private CartService cartService;
 	
+	private int currentPage = 1;
+	private int blockCount = 4;
+	private int blockPage = 1;
+	private Paging page;
+	
+	
 	@RequestMapping("/main.conn")
 	public ModelAndView mainList(CommandMap commandMap, HttpSession session)throws Exception{
 		ModelAndView mv = new ModelAndView();
 		
-		List<Map<String, Object>> bestList = mainService.GoodsBestList().subList(0, 8);
-		List<Map<String, Object>> saleList = mainService.GoodsSaleList1().subList(0, 8);
+		List<Map<String, Object>> bestList = mainService.GoodsBestList();
+		List<Map<String, Object>> saleList = mainService.GoodsSaleList1();
 		List<Map<String, Object>> smallcategoryList = goodsService.smallCategoryList();
 		int count = 0;
 		
@@ -46,6 +52,14 @@ public class MainController {
 			session.setAttribute("count", count);
 		}
 		
+		int totalCount = 8;
+		page = new Paging(currentPage, totalCount, blockCount, blockPage, "main.conn");
+		int lastCount = totalCount;
+		if (page.getEndCount() < totalCount)
+			lastCount = page.getEndCount() + 1;
+		bestList = bestList.subList(page.getStartCount(), lastCount);
+		saleList = saleList.subList(page.getStartCount(), lastCount);
+		
 		mv.addObject("MEMBER_NUM", session.getAttribute("MEMBER_NUM"));
 		mv.addObject("smallcategoryList", smallcategoryList);
 		mv.addObject("bestList", bestList);
@@ -54,9 +68,7 @@ public class MainController {
 		return mv;
 	}
 	
-	
-	
-	
+
 	@RequestMapping("eventList.conn")
 	public ModelAndView eventList(CommandMap commandMap)throws Exception{
 		ModelAndView mv = new ModelAndView("/event/eventList");
